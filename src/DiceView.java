59411f01ef7fe66e80792e1f07445e58d8f841b0
Gina Sprint <sprint@gonzaga.edu>
2019-10-15 14:25:40 -0700

    initial

---
+++ src/DiceView.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class DiceView extends JFrame {
+    // task: create a JFrame with a Y_AXIS boxlayout
+    // that has two X_AXIS boxlayouts
+    // inside have 3 labels and one button
+    // click the "roll!" button, and update the first two
+    // labels to show random die rolls in [1, 6]
+    // update the third label to show the sum
+    // ______________
+    //|  5       3   |
+    //
+    //| Roll!   Sum:8|
+    // _______________
+
+    public DiceView() {
+        super("Dice Thrower");
+
+
+    }
+}
